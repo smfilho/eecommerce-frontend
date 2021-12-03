79a43b3ecafa9157025367a8e15d4f83f8d569ba
Silvio Dev <developer.silvio@gmail.com>
2021-12-03 16:10:12 -0600

    added: pages

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function indexPage() {
+  return (
+    <div>
+      <h1>Index page</h1>
+    </div>
+  );
+}
---
+++ pages/sell.js
@@ -0,0 +1,7 @@
+export default function sellPage() {
+  return (
+    <div>
+      <h1>Sell page</h1>
+    </div>
+  );
+}
